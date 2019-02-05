232c41c96753c6dbf26739272ce08a7152fb77db
debrashere <debrashere@gmail.com>
2019-02-04 22:50:36 -0700

    server test updates

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 module.exports = {
     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://immense-savannah-21266.herokuapp.com/',
     DATABASE_URL: process.env.DATABASE_URL || 'mongodb://mlab01User:Fit4newyear@ds121135.mlab.com:21135/myeducationprod',
-    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'mongodb://mlab01User:Fit4newyear@ds121135.mlab.com:21135/myeducationprod',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'mongodb://mlab01User:Fit4newyear@ds221115.mlab.com:21115/myeducationtest',
     PORT:process.env.PORT || 21135,
     TEST_PORT:process.env.TEST_PORT || 21115,
     JWT_SECRET:process.env.JWT_SECRET || 'mYS#CretSt*ng',
